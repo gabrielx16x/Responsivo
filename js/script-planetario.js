a79a1e8fc0d3b41064ca611ab8998d6647405b26
Biel <gabriel.valentim62@aluno.ifce.edu.br>
2023-05-28 21:32:12 -0300

    ultima versão

--- js/script-planetario.js
+++ js/script-planetario.js
@@ -2,5 +2,5 @@ function alterarParaPortugues() {
     window.location.href = "../html/planetario.html";
   }
 function alterarParaIngles() {
-    window.location.href = "../html/planetario_en";
+    window.location.href = "../html/planetario_en.html";
   }
